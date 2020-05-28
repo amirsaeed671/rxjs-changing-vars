ae78d99567b655b17e7bf91f54c0d847dc7708e5
Amir Ali <amir.saeed.ali92@gmail.com>
2020-05-28 18:06:26 +0500

    latest build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import { of } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { pluck } from 'rxjs/operators';
 
-of(1, 2, 3).subscribe(console.log);
+const control = document.querySelector('.controls');
+
+const source$ = fromEvent(control, 'change').pipe(
+    pluck('target')
+);
+
+source$.subscribe(element => {
+    const suffix = element.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${element.name}`, element.value + suffix);
+});
